c502aa427a927f6bb7b1ffb9723dcfdaa15a26de
GuiTheMalves <guiegustavolondrina@gmail.com>
2025-12-04 16:57:24 -0300

    Update EmissorNúmeroAnoComparator.java

--- src/br/edu/ifpr/gep/model/utils/comparators/EmissorNúmeroAnoComparator.java
+++ src/br/edu/ifpr/gep/model/utils/comparators/EmissorNúmeroAnoComparator.java
@@ -1,14 +1,11 @@
 package br.edu.ifpr.gep.model.utils.comparators;
 
-import java.time.LocalDate;
 import java.util.Comparator;
 
 import br.edu.ifpr.gep.model.Portaria;
-import br.edu.ifpr.gep.model.utils.ParseValores;
 
 //Comparator que ordena Portarias seguindo a hierarquia: emissor, número, ano
 public class EmissorNúmeroAnoComparator implements Comparator<Portaria> {
-    private final ParseValores pars = new ParseValores();
 	@Override
 	//Compara duas portarias pela ordem estabelecida
     public int compare(Portaria o1, Portaria o2) {	
@@ -21,10 +18,8 @@ public class EmissorNúmeroAnoComparator implements Comparator<Portaria> {
 		comp = o1.getNúmero().compareTo(o2.getNúmero());
 		if (comp != 0)
 			return comp;
-		LocalDate data1 = pars.ParseData(o1.getPublicação());
-		LocalDate data2 = pars.ParseData(o1.getPublicação());
 
 		//Se emissor e número forem iguais, compara o ano da publicação
-		return Integer.compare(data1.getYear(),data2.getYear());
+		return Integer.compare(o1.getPublicação().getYear(),o2.getPublicação().getYear());
 	}
 }
